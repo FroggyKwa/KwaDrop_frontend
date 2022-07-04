edf5cb3125efde82bd9e501fbc3c14c7d4fc71d9
FroggyKwa <ilya.laguxa@gmail.com>
2022-07-05 02:17:50 +1000

    add header tag

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,29 +1,31 @@
 <template>
-  <v-app-bar shrink-on-scroll id="bar" elevation="0">
-    <v-btn v-ripple="false" @click="drawer = !drawer"
-           class="d-flex d-sm-none">
-      <font-awesome-icon id="dropdown-caret-icon" size="2x" icon="fa-solid fa-caret-down"/>
-    </v-btn>
-    <router-link to="/">
-      <div id="logo">{{ appBarTitle }}</div>
-    </router-link>
-    <v-container class="d-none d-sm-block">
-      <v-row class="float-end nav-buttons">
-        <v-col v-for="item in items" :key="item">
-          <router-link to="about">{{ item }}</router-link>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app-bar>
-  <v-navigation-drawer color="$not-white" v-model="drawer" absolute temporary=true>
-    <v-item-group id="left-menu">
-      <v-list-item v-for="item in items" :key="item">
-        <v-list-item-title>
-          <router-link :to="item.toLowerCase()" class="left-menu-item">{{ item }}</router-link>
-        </v-list-item-title>
-      </v-list-item>
-    </v-item-group>
-  </v-navigation-drawer>
+  <header>
+    <v-app-bar shrink-on-scroll id="bar" elevation="0">
+      <v-btn v-ripple="false" @click="drawer = !drawer"
+             class="d-flex d-sm-none">
+        <font-awesome-icon id="dropdown-caret-icon" size="2x" icon="fa-solid fa-caret-down"/>
+      </v-btn>
+      <router-link to="/">
+        <div id="logo">{{ appBarTitle }}</div>
+      </router-link>
+      <v-container class="d-none d-sm-block">
+        <v-row class="float-end nav-buttons">
+          <v-col v-for="item in items" :key="item">
+            <router-link to="about">{{ item }}</router-link>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <v-navigation-drawer color="$not-white" v-model="drawer" absolute temporary=true>
+      <v-item-group id="left-menu">
+        <v-list-item v-for="item in items" :key="item">
+          <v-list-item-title>
+            <router-link :to="item.toLowerCase()" class="left-menu-item">{{ item }}</router-link>
+          </v-list-item-title>
+        </v-list-item>
+      </v-item-group>
+    </v-navigation-drawer>
+  </header>
 </template>
 <script lang='ts'>
 import { defineComponent } from 'vue';
